81e77eb2c717e79ba3f1fdd81cfe9b98c1a2a869
Donna Mayfield <cdmayfield23@gmail.com>
2018-05-21 02:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ function dec(n){
 }
 
 function makeInt(n){
-  return parseInt(0x2328)
+  return parseInt(n,10)
 }
 
 function preserveDecimal(n){
-  return parseFloat(2.222)
+  return parseFloat(n)
 }
